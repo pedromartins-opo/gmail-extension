8faf5700e363a93a2621f602a3044af30caedef2
Pedro Martins <pedromartins.opo@gmail.com>
2022-01-30 15:22:16 +0000

    Change modal to dropdown

--- src/myapp.js
+++ src/myapp.js
@@ -7,10 +7,14 @@ InboxSDK.load(2, 'YOUR_APP_ID_HERE').then(function(sdk){
             iconUrl: 'http://localhost:3000/ic-template.png',
             onClick: function(event) {
                 console.log(event);
-                var modal = sdk.Widgets.showModalView({
-                    title: 'Templates',
-                    //'el': '<div id="inbox-sdk-template-modal">teste</div>',
-                    'el': '<iframe style="width: 640px; height: 480px;" frameBorder="0" src="http://localhost:3000/" title="description"></iframe>'
+
+                var dropdown = event.dropdown;
+                dropdown.el.innerHTML = '<iframe style="width: 350px; height: 400px;" frameBorder="0" src="http://localhost:3000/" title="description"></iframe>';
+                dropdown.setPlacementOptions({
+                    position: 'top',
+                    hAlign  : 'left',
+                    forcePosition: true,
+                    forceHAlign: true,
                 });
 
                 window.onmessage = function(e) {
@@ -19,10 +23,19 @@ InboxSDK.load(2, 'YOUR_APP_ID_HERE').then(function(sdk){
                         // it will insert the text of the template into the body of the draft (replacing any existing text there).
                         event.composeView.setBodyText(e.data);
                         //event.composeView.insertTextIntoBodyAtCursor(e.data);
-                        modal.close();
+                        dropdown.close();
                     }
                 };
+
+                /*
+                var modal = sdk.Widgets.showModalView({
+                    title: 'Templates',
+                    //'el': '<div id="inbox-sdk-template-modal">teste</div>',
+                    'el': '<iframe style="width: 640px; height: 480px;" frameBorder="0" src="http://localhost:3000/" title="description"></iframe>'
+                });
+                */
             },
+            hasDropdown: true
         });
     });
 });
